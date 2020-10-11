28fe423b604fd0a32ee263a6b3f91e99b4ea5ede
Joe Pena <joempena26@gmail.com>
2020-10-11 14:09:41 -0600

    Clean up homepage links

--- the_loof/apps/public/admin.py
+++ the_loof/apps/public/admin.py
@@ -3,13 +3,19 @@ from .models import Article
 
 # Register your models here.
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ("headline", "slug", "created")
+    list_display = (
+        "headline",
+        "slug",
+        "article_type",
+        "created",
+    )
     list_filter = ("created",)
     search_fields = (
         "headline",
         "body",
         "instruments__symbol",
         "instruments__company_name",
+        "byline",
     )
     prepopulated_fields = {"slug": ("headline",)}
 
